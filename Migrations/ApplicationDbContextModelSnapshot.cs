60bcda2110808b93571ddadecd7473d61c8afbf6
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-02-25 12:03:08 +0700

    fix: change version ef core 5.0.17 to 5.0.9

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Assignment2.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.17")
+                .HasAnnotation("ProductVersion", "5.0.9")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("Assignment2.Models.Category", b =>
@@ -27,7 +27,8 @@ namespace Assignment2.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)")
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)")
                         .HasColumnName("Cat_Name");
 
                     b.HasKey("ID");
@@ -65,11 +66,19 @@ namespace Assignment2.Migrations
                         .HasColumnName("cat_id");
 
                     b.Property<string>("Description")
-                        .HasColumnType("nvarchar(max)")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)")
                         .HasColumnName("pro_description");
 
+                    b.Property<int>("Likes")
+                        .HasColumnType("int")
+                        .HasColumnName("Likes");
+
                     b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)")
                         .HasColumnName("pro_name");
 
                     b.Property<long>("Price")
