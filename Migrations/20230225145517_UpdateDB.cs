5d5ba846eb5c880dd29114943fe73727ebf9979d
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-02-26 00:40:35 +0700

    db: add column imageUrl

---
+++ Migrations/20230225145517_UpdateDB.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment2.Migrations
+{
+    public partial class UpdateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Likes",
+                table: "Products",
+                newName: "likes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "likes",
+                table: "Products",
+                newName: "Likes");
+        }
+    }
+}
